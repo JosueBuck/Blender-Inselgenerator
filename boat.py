e54174709f4080849070df39cbbcfcbb00be4359
ThyraHultsch <thyra.hultsch@arcor.de>
2022-02-07 21:44:37 +0100

    Add Boat animation and fixed random position

--- boat.py
+++ boat.py
@@ -1,6 +1,7 @@
 import bpy
 import random
 import decimal
+import math
 
 class Boat():
 
@@ -32,10 +33,50 @@ class Boat():
             link_to.objects.link(empty)
             print("ISLAND SIZE")
             print(_islandSize)
-            
-            empty.location.x = float(decimal.Decimal(random.randrange(_islandSize*100, 490))/100)
-            empty.location.y = float(decimal.Decimal(random.randrange(_islandSize*100, 490))/100)
             empty.location.z = 0.07
             empty.scale.x = 0.1
             empty.scale.y = 0.1
             empty.scale.z = 0.1
+
+            #_self.randomLocation(empty, _islandSize)
+            _self.animateBoat(empty, _islandSize)
+            
+
+    def animateBoat(self, empty, _islandSize):
+        frames = 250
+        i = 0
+        angle = 0
+        _islandSize += 1
+        rotationAngle = 0
+        
+        
+        while i <= frames:
+            empty.location.x = float(decimal.Decimal(math.cos(angle)*_islandSize)) 
+            empty.location.y = float(decimal.Decimal(math.sin(angle)*_islandSize)) 
+            empty.keyframe_insert(data_path="location",frame=i)
+            empty.rotation_euler[2] = rotationAngle
+            empty.keyframe_insert("rotation_euler",frame=i)
+
+            angle = angle -  0.244346
+            rotationAngle = rotationAngle - 0.244346
+            i += 10
+        
+    def randomLocation(self, empty, _islandSize): 
+        locationX = float(decimal.Decimal(random.randrange(_islandSize*100, 470))/100)
+        locationY = float(decimal.Decimal(random.randrange(_islandSize*100, 470))/100)
+
+        probX = random.randrange(1, 100)
+        probY = random.randrange(1, 100)
+
+        if (probX > 50):
+            locationX = locationX * -1
+        if (probY > 50):
+            locationY = locationY * -1
+        
+        empty.location.x = locationX
+        empty.location.y = locationY
+        empty.rotation_euler[2] = random.randrange(0, 360)
+        empty.location.z = 0.07
+        empty.scale.x = 0.1
+        empty.scale.y = 0.1
+        empty.scale.z = 0.1
