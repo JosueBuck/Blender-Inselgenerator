1ea035a3d208dd33eb0dc9dc619c6ae79e133a79
lukas <willmann.Lukas@web.de>
2022-02-09 10:26:54 +0100

    unlinked Iceshell from main collection

--- water.py
+++ water.py
@@ -48,17 +48,15 @@ class Water():
         for c_curve in mat_water.node_tree.animation_data.action.fcurves:
             for c_keyframe in c_curve.keyframe_points:
                 c_keyframe.interpolation = "LINEAR"
-
-        print(_season)
         
         if(_season == "3"):
-            _self.createIceShell(1)
+            iceShellObject = _self.createIceShell(1)
             bpy.context.view_layer.objects.active = bpy.data.objects.get("Plane")
             bpy.ops.object.particle_system_add()
             bpy.data.particles["ParticleSettings"].count = 100
             bpy.data.particles["ParticleSettings"].type = 'HAIR'
             bpy.data.particles["ParticleSettings"].render_type = 'OBJECT'
-            bpy.data.particles["ParticleSettings"].instance_object = bpy.data.objects["Sphere.001"]
+            bpy.data.particles["ParticleSettings"].instance_object = iceShellObject
             bpy.data.particles["ParticleSettings"].particle_size = 0.07
             bpy.data.particles["ParticleSettings"].hair_length = 2
             bpy.data.particles["ParticleSettings"].rotation_mode = 'OB_Y'
@@ -66,11 +64,21 @@ class Water():
             sky = Sky()
             sky.createSky()
 
-    def createIceShell(_self, _number):
-        bpy.ops.mesh.primitive_uv_sphere_add(segments=30, ring_count=30,enter_editmode=True, align='WORLD', scale=(1,1,0.5))
-        bpy.ops.object.editmode_toggle()
+    def createIceShell(_self, _number) -> object:
+        iceCollection = bpy.context.blend_data.collections.new(name='ice_collection')
+
+        bpy.ops.mesh.primitive_uv_sphere_add(segments=30, ring_count=30, align='WORLD', scale=(1,1,0.5))
         tex = bpy.data.textures.new(name = "cloud" + str(_number), type="CLOUDS")
         tex.noise_scale = random.uniform(1.5,2)
         tex.noise_depth = 1
-        bpy.context.object.modifiers.new(name="DisplaceShells", type='DISPLACE')
-        bpy.context.object.modifiers['DisplaceShells'].texture = tex
+        iceShellObject = bpy.context.object
+        iceShellObject.modifiers.new(name="DisplaceShells", type='DISPLACE')
+        iceShellObject.modifiers['DisplaceShells'].texture = tex
+        iceShellObject.name = "Ice Shell"
+
+        iceCollection.objects.link(iceShellObject)
+        bpy.data.collections["Collection"].objects.unlink(iceShellObject)
+
+        return iceShellObject
+
+        
